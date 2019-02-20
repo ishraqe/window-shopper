0201b2f4f47e618716036780ebd23c12d464bc4a
Ishraqe Manjur <ishraqemanjur@Ishraqes-MacBook-Pro.local>
2019-02-20 16:45:16 +0600

    input buttons not working

---
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Ishraqe Manjur on 20/2/19.
+//  Copyright © 2019 Ishraqe Manjur. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
